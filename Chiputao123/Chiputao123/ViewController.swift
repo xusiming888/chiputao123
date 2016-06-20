40ffc64b6be32025e5b682d412d069dae805ef65
xusiming888 <siming_xu@emerson.edu>
2016-06-20 18:15:42 -0400

    first trying

---
+++ Chiputao123/Chiputao123/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chiputao123
+//
+//  Created by Mr.Xu on 16/6/20.
+//  Copyright © 2016年 Mr.Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
